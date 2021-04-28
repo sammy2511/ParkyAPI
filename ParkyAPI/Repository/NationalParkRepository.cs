55cd8890055e95ed1251df019da4b1d532310898
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2021-04-28 13:11:33 +0530

    Implemented Repository pattern

--- ParkyAPI/Repository/NationalParkRepository.cs
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -1,4 +1,5 @@
-﻿using ParkyAPI.Models;
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
 using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
@@ -6,47 +7,61 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ParkyAPI.Repository
+
+
 {
     public class NationalParkRepository : INationalParkRepository
     {
+
+        private readonly ApplicationDBContext _db;
+
+        public NationalParkRepository(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public bool CreateNationalPark(NationalPark nationalPark)
         {
-            throw new NotImplementedException();
+            _db.NationalParks.Add(nationalPark);
+            return Save();
         }
 
         public bool DeleteNationalPark(NationalPark nationalPark)
         {
-            throw new NotImplementedException();
+            _db.NationalParks.Remove(nationalPark);
+            return Save();
         }
 
         public NationalPark GetNationalPark(int nationalParkId)
         {
-            throw new NotImplementedException();
+            return _db.NationalParks.FirstOrDefault(a => a.Id == nationalParkId);
         }
 
         public ICollection<NationalPark> GetNationalParks()
         {
-            throw new NotImplementedException();
+            return _db.NationalParks.OrderBy(a => a.Name).ToList();
         }
 
         public bool NationalParkExists(string name)
         {
-            throw new NotImplementedException();
+            bool value = _db.NationalParks.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
         }
 
         public bool NationalParkExists(int nationalParkId)
         {
-            throw new NotImplementedException();
+            bool value = _db.NationalParks.Any(a => a.Id == nationalParkId);
+            return value;
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            return _db.SaveChanges() >= 0 ? true : false;
         }
 
         public bool UpdateNationalPark(NationalPark nationalPark)
         {
-            throw new NotImplementedException();
+            _db.NationalParks.Update(nationalPark);
+            return Save();
         }
     }
 }
