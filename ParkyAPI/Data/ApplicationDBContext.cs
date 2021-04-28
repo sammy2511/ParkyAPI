df58889f13ae165b12adc7d71e54aa7530748760
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2021-04-28 12:37:27 +0530

    initial comit

---
+++ ParkyAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext( DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
