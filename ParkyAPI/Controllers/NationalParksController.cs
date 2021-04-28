cf71093dc208907fdade33809f781a2402deefc0
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2021-04-28 15:13:15 +0530

    Post API Implementation

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
 using ParkyAPI.Models.DTOs;
 using ParkyAPI.Repository.IRepository;
 using System;
@@ -36,6 +37,51 @@ namespace ParkyAPI.Controllers
                 objDto.Add(_mapper.Map<NationalParkDTO>(obj));
             }
             return Ok(objDto);
+        }
+
+        [HttpGet("{nationalParkid:int}")]
+        public IActionResult GetNationalPark(int nationalParkid)
+        {
+            var obj = _npRepo.GetNationalPark(nationalParkid);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            var objDto = _mapper.Map<NationalParkDTO>(obj);
+
+            return Ok(objDto);
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationaPark([FromBody] NationalParkDTO nationalParkDTO)
+        {
+            if(nationalParkDTO == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_npRepo.NationalParkExists(nationalParkDTO.Name))
+            {
+                ModelState.AddModelError("", "National Park Already Exists!");
+                return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDTO);
+
+            if (!_npRepo.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong while adding {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok();
         }
     }
 }
